c4b1db1b6167652f52fa1b258dcafc6bc3941a58
yuvalsidis <yuvalsidis@gmail.com>
2024-06-04 23:18:45 +0300

    Add scrolling via navigation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,23 @@
 
-import { Children } from "react";
 import "./globals.css";
+import { Children } from "react";
+import Link from 'next/link';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function RootLayout({ children }) {
+ 
+  
   return (
     <html lang="en">
       <body>
         <header>
           <h3 className="logo">Yuval.dev</ h3>
           <nav>
-            <a href="#Home">Home</a>
-            <a href="#About">About</a>
-            <a href="#Project">Projects</a>
-            <a href="#  Contact">Contact</a>
+            <Link href="#home" >Home</Link>
+            <Link href="#about">About</Link>
+            <Link href="#projects">Projects</Link>
+            <Link href="#contact">Contact</Link>
           </nav>
         </header>
         <main>{children}</main>
