05d323d7ca1ba5220a4d42b2fc2862fa76b0154b
yuvalsidis <yuvalsidis@gmail.com>
2024-06-10 00:55:01 +0300

    Fix deploy

--- src/app/cmps/MainSection.jsx
+++ src/app/cmps/MainSection.jsx
@@ -1,6 +1,5 @@
 // MainSection.jsx
 import React from 'react';
-import styles from '../styles/MainSection.module.scss'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
@@ -9,6 +8,8 @@ import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import Image from 'next/image';
 import EmailLink from './EmailLink'
 import ProjectSection from './ProjectSection';
+import styles from '../styles/MainSection.module.scss'
+
 
 function MainSection() {
   return (
