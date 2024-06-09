b537f9c7b2c2f4decca48c1fb4f635b6242e5a4d
yuvalsidis <yuvalsidis@gmail.com>
2024-06-09 22:17:12 +0300

    Add projects section with instegram project layout and style

--- src/app/cmps/ProjectSection.jsx
+++ src/app/cmps/ProjectSection.jsx
@@ -1,8 +1,24 @@
+import styles from '../styles/ProjectSection.module.scss'
+import Image from 'next/image'
 
-const ProjectSection = () =>{
+const ProjectSection = () => {
 
-    return(
-        <h1> project Section</h1>
+    return (
+        <div>
+            <div className={styles.ProjectSection}>
+                <Image width={416} height={520} src='/img/instagram.png' className={styles.InstagramProjectImg}></Image>
+                <div className={styles.ProjectContent}>
+                    <h2>Instegram Project</h2>
+                    <div className={styles.ProjectDescription}>
+                        I've developed a social platform similar to Instagram,
+                        enabling users to upload, share, and comment on images.
+                        This project, built with React, SCSS, Vite, and Redux,
+                        showcases a seamless user experience with demo data to illustrate its functionality.
+                    </div>
+                    <a className='link'>https://github.com/yuvalsidis/Yuvigram-frontend-instegram</a>
+                </div>
+            </div>
+        </div>
     )
 }
 
