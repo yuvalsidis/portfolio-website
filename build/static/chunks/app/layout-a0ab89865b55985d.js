f8db26ccbf252dd82683b63a8cf7fb6d21cd32dd
yuvalsidis <yuvalsidis@gmail.com>
2024-06-10 16:33:02 +0300

    Remove build and out directory

--- build/static/chunks/app/layout-a0ab89865b55985d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7499:function(e,n,i){Promise.resolve().then(i.bind(i,3642)),Promise.resolve().then(i.bind(i,1364)),Promise.resolve().then(i.t.bind(i,231,23)),Promise.resolve().then(i.t.bind(i,3054,23))},3054:function(){}},function(e){e.O(0,[141,674,231,971,23,744],function(){return e(e.s=7499)}),_N_E=e.O()}]);
