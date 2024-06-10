c319b12588921b8714ef596ae18f33e0fe9e4764
yuvalsidis <yuvalsidis@gmail.com>
2024-06-10 16:51:39 +0300

    Chane nextconfigjs

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // Optional: Customize Webpack configuration
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-    });
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
