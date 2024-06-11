a5dbd2904ac6126bcf7b08504dc2b7abc2de603d
yuvalsidis <yuvalsidis@gmail.com>
2024-06-11 22:06:30 +0300

    Update ProjectSection

--- src/app/cmps/ProjectSection.jsx
+++ src/app/cmps/ProjectSection.jsx
@@ -10,12 +10,12 @@ const ProjectSection = () => {
                 <Image width={416} height={520} src='/img/instagram.png' className={styles.InstagramProjectImg}></Image>
                 <div className={styles.ProjectContent}>
                     <h2>Instagram Project</h2>
-                    <div className={styles.ProjectDescription}>
+                    <p className={styles.ProjectDescription}>
                         I've developed a social platform similar to Instagram,
                         enabling users to upload, share, and comment on images.
                         This project, built with React, SCSS, Vite, and Redux,
                         showcases a seamless user experience with demo data to illustrate its functionality.
-                    </div>
+                    </p>
                     <Link href='https://github.com/yuvalsidis/Yuvigram-frontend-instegram'>Github Repository Link</Link>
                 </div>
             </div>
